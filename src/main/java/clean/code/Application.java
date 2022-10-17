8a2164ed0562f604337161060b0e1b3f67eab078
Pbonnamy <bonnamy.77@gmail.com>
2022-10-17 18:03:10 +0200

    Health controller

--- src/main/java/clean/code/Application.java
+++ src/main/java/clean/code/Application.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Application {
     public static void main(String[] args) {
+        System.setProperty("server.servlet.context-path", "/api/v1");
         SpringApplication.run(Application.class, args);
         // TODO application
     }
