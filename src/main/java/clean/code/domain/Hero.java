828a36fcddb1a9f062d849d09964c72ed3749a8e
Pierre <bonnamy.77@gmail.com>
2022-11-06 18:58:18 +0100

    using lombok for the Hero class

--- src/main/java/clean/code/domain/Hero.java
+++ src/main/java/clean/code/domain/Hero.java
@@ -1,70 +1,22 @@
 package clean.code.domain;
 
 
-// TODO Refactor
-final class Hero {
+import lombok.*;
+import lombok.experimental.FieldDefaults;
 
-    private final String name;
-    private final int hp;
-    private final int xp;
-    private final int power;
-    private final Armor armor;
-    private final Speciality speciality;
-    private final Rarity rarity;
-    private final int level;
-
-    private Hero(
-            String name,
-            int hp,
-            int xp,
-            int power,
-            Armor armor,
-            Speciality speciality,
-            Rarity rarity,
-            int level) {
-        this.name = name;
-        this.hp = hp;
-        this.xp = xp;
-        this.power = power;
-        this.armor = armor;
-        this.speciality = speciality;
-        this.rarity = rarity;
-        this.level = level;
-    }
-
-
-    //<editor-fold desc="getters">
-    public String name() {
-        return name;
-    }
-
-    public int hp() {
-        return hp;
-    }
-
-    public int xp() {
-        return xp;
-    }
-
-    public int power() {
-        return power;
-    }
-
-    public Armor armor() {
-        return armor;
-    }
-
-    public Speciality speciality() {
-        return speciality;
-    }
-
-    public Rarity rarity() {
-        return rarity;
-    }
-
-    public int level() {
-        return level;
-    }
-    //</editor-fold>
-
+@Getter
+@With
+@FieldDefaults(makeFinal = true, level = AccessLevel.PRIVATE)
+@AllArgsConstructor
+@ToString
+@EqualsAndHashCode
+public final class Hero {
+    String name;
+    int hp;
+    int xp;
+    int power;
+    Armor armor;
+    Speciality speciality;
+    Rarity rarity;
+    int level;
 }
