7e8a7c0e7efa91344814136feb171aeec8d89549
Pierre <bonnamy.77@gmail.com>
2023-02-09 00:47:05 +0100

    bootstraping & mongodb

---
+++ src/main/java/clean/code/bootstrap/config/domain/DomainConfiguration.java
@@ -0,0 +1,16 @@
+package clean.code.bootstrap.config.domain;
+
+import clean.code.domain.functional.service.HeroCreatorService;
+import clean.code.domain.ports.client.HeroCreatorApi;
+import clean.code.domain.ports.server.HeroPersistenceSpi;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class DomainConfiguration {
+
+    @Bean
+    public HeroCreatorApi heroCreatorService(HeroPersistenceSpi spi) {
+        return new HeroCreatorService(spi);
+    }
+}
--- src/main/java/clean/code/server/adapter/HeroDatabaseAdapter.java
+++ src/main/java/clean/code/server/adapter/HeroDatabaseAdapter.java
@@ -1,4 +1,36 @@
 package clean.code.server.adapter;
 
-public class HeroDatabaseAdapter {
+import clean.code.domain.ApplicationError;
+import clean.code.domain.functional.model.Hero;
+import clean.code.domain.ports.server.HeroPersistenceSpi;
+import clean.code.server.repository.HeroRepository;
+import io.vavr.control.Either;
+import java.util.Collections;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+@RequiredArgsConstructor
+@Service
+public class HeroDatabaseAdapter implements HeroPersistenceSpi {
+
+    private final HeroRepository repository;
+
+    @Override
+    public Either<ApplicationError, Hero> save(Hero hero) {
+        return null;
+    }
+
+    @Override
+    public List<Hero> findAll() {
+        return Collections.emptyList();
+    }
+
+    @Override
+    public Optional<Hero> findById(UUID id) {
+        return Optional.empty();
+    }
 }
