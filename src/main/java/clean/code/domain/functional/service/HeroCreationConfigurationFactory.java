2ae5f709352db67b12e3a1a51dcd5eae94b50e6b
Pierre <bonnamy.77@gmail.com>
2023-02-27 00:28:00 +0100

    bonus for combat & creation

--- src/main/java/clean/code/domain/functional/service/HeroCreationConfigurationFactory.java
+++ src/main/java/clean/code/domain/functional/service/HeroCreationConfigurationFactory.java
@@ -1,12 +1,22 @@
 package clean.code.domain.functional.service;
 
 public interface HeroCreationConfigurationFactory {
-    static HeroCreationConfiguration forSpeciality(String speciality) {
+    static HeroCreationConfiguration forSpeciality(String speciality, String rarity) {
+        double rarityBonus = forRarity(rarity);
         return switch (speciality) {
-            case "TANK" -> new HeroCreationConfiguration(1000, 100, 20);
-            case "ASSASSIN" -> new HeroCreationConfiguration(800, 200, 5);
-            case "WIZARD" -> new HeroCreationConfiguration(700, 150, 10);
+            case "TANK" -> new HeroCreationConfiguration(1000 + (int) (1000 * rarityBonus), 100 + (int) (100 * rarityBonus), 20 + (int) (20 * rarityBonus));
+            case "ASSASSIN" -> new HeroCreationConfiguration(800 + (int) (800 * rarityBonus), 200 + (int) (200 * rarityBonus), 5 + (int) (5 * rarityBonus));
+            case "WIZARD" -> new HeroCreationConfiguration(700 + (int) (700 * rarityBonus), 150 + (int) (150 * rarityBonus), 10 + (int) (10 * rarityBonus));
             default -> throw new IllegalArgumentException("Unknown speciality");
+        };
+    }
+
+    private static double forRarity(String rarity) {
+        return switch (rarity) {
+            case "LEGENDARY" -> 0.2;
+            case "RARE" -> 0.1;
+            case "COMMON" -> 0;
+            default -> throw new IllegalArgumentException("Unknown rarity");
         };
     }
 }
--- src/test/java/clean/code/domain/functional/service/HeroCreationConfigurationFactoryTest.java
+++ src/test/java/clean/code/domain/functional/service/HeroCreationConfigurationFactoryTest.java
@@ -11,8 +11,17 @@ class HeroCreationConfigurationFactoryTest {
 
     @ParameterizedTest
     @ValueSource(strings = {"TANK", "ASSASSIN", "WIZARD"})
-    void should_contains_good_values(String speciality) {
-        var conf = HeroCreationConfigurationFactory.forSpeciality(speciality);
+    void should_contains_good_values_for_speciality(String speciality) {
+        var conf = HeroCreationConfigurationFactory.forSpeciality(speciality, "COMMON");
+        Assertions.assertThat(conf.hpAtLevel1()).isPositive();
+        Assertions.assertThat(conf.armorAtLevel1()).isPositive();
+        Assertions.assertThat(conf.powerAtLevel1()).isPositive();
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"COMMON", "RARE", "LEGENDARY"})
+    void should_contains_good_values_for_rarity(String rarity) {
+        var conf = HeroCreationConfigurationFactory.forSpeciality("TANK", rarity);
         Assertions.assertThat(conf.hpAtLevel1()).isPositive();
         Assertions.assertThat(conf.armorAtLevel1()).isPositive();
         Assertions.assertThat(conf.powerAtLevel1()).isPositive();
@@ -20,8 +29,14 @@ class HeroCreationConfigurationFactoryTest {
 
     @ParameterizedTest
     @ValueSource(strings = {"NEW", "HELLO", "MAGICIAN"})
-    void should_throw_exception(String speciality) {
-        assertThrows(IllegalArgumentException.class, () -> HeroCreationConfigurationFactory.forSpeciality(speciality));
+    void should_throw_exception_for_unknown_speciality(String speciality) {
+        assertThrows(IllegalArgumentException.class, () -> HeroCreationConfigurationFactory.forSpeciality(speciality, "COMMON"));
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"NEW", "HELLO", "MAGICIAN"})
+    void should_throw_exception_for_unknown_rarity(String rarity) {
+        assertThrows(IllegalArgumentException.class, () -> HeroCreationConfigurationFactory.forSpeciality("TANK", rarity));
     }
 
 }
