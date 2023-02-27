3d66d680de7919538e872fae5d97c8b401bd2b0a
Pierre <pbonnamy.pro@gmail.com>
2023-02-27 11:24:33 +0100

    test

--- src/main/java/clean/code/client/controller/HealthController.java
+++ src/main/java/clean/code/client/controller/HealthController.java
@@ -7,6 +7,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class HealthController {
     @GetMapping("/health")
     public String health() {
-        return "OK";
+        return "OK - Health";
     }
 }
