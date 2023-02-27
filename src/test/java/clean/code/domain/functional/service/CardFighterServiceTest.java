6099b3e640442ff89d22793a06fae3f809d8a56b
Nigth <omnes.theo@gmail.com>
2023-02-27 14:02:22 +0100

    fix tests

--- src/test/java/clean/code/domain/functional/service/CardFighterServiceTest.java
+++ src/test/java/clean/code/domain/functional/service/CardFighterServiceTest.java
@@ -3,7 +3,9 @@ package clean.code.domain.functional.service;
 import clean.code.domain.ApplicationError;
 import clean.code.domain.functional.model.Card;
 import clean.code.domain.functional.model.FightResult;
+import clean.code.domain.functional.model.Player;
 import clean.code.domain.ports.server.CardPersistenceSpi;
+import clean.code.domain.ports.server.PlayerPersistenceSpi;
 import io.vavr.control.Either;
 import io.vavr.control.Option;
 import lombok.val;
@@ -23,30 +25,41 @@ class CardFighterServiceTest {
     @InjectMocks CardFighterService cardFighterService;
 
     @Mock
-    CardPersistenceSpi spi;
+    CardPersistenceSpi cardSpi;
+
+    @Mock
+    PlayerPersistenceSpi playerSpi;
 
     @Test
     void should_fight() {
         val attackerId = UUID.randomUUID();
         val defenderId = UUID.randomUUID();
 
-        val attacker = Card.builder().id(attackerId).hp(10).armor(10).power(10).build();
+        val givenPlayer = Player.builder().id(UUID.randomUUID()).build();
+
+        val attacker = Card.builder().id(attackerId).hp(10).armor(10).power(10)
+                .playerId(givenPlayer.getId().toString()).build();
         val defender = Card.builder().id(defenderId).hp(5).armor(5).power(5).build();
 
-        when(spi.findById(attackerId)).thenReturn(Option.of(attacker));
-        when(spi.findById(defenderId)).thenReturn(Option.of(defender));
 
-        when(spi.save(any(Card.class))).thenReturn(Either.right(attacker));
+
+        when(cardSpi.findById(attackerId)).thenReturn(Option.of(attacker));
+        when(cardSpi.findById(defenderId)).thenReturn(Option.of(defender));
+
+        when(playerSpi.findById(UUID.fromString(attacker.getPlayerId()))).thenReturn(Option.of(givenPlayer));
+
+        when(cardSpi.save(any(Card.class))).thenReturn(Either.right(attacker));
+        when(playerSpi.save(any(Player.class))).thenReturn(Either.right(givenPlayer));
 
         val actual = cardFighterService.fight(attackerId, defenderId);
 
         VavrAssertions.assertThat(actual).containsRightInstanceOf(FightResult.class);
 
-        verify(spi).findById(attackerId);
-        verify(spi).findById(defenderId);
-        verify(spi).save(any(Card.class));
+        verify(cardSpi).findById(attackerId);
+        verify(cardSpi).findById(defenderId);
+        verify(cardSpi).save(any(Card.class));
 
-        verifyNoMoreInteractions(spi);
+        verifyNoMoreInteractions(cardSpi);
     }
 
     @Test
@@ -54,15 +67,15 @@ class CardFighterServiceTest {
         val attackerId = UUID.randomUUID();
         val defenderId = UUID.randomUUID();
 
-        when(spi.findById(attackerId)).thenReturn(Option.none());
+        when(cardSpi.findById(attackerId)).thenReturn(Option.none());
 
         val actual = cardFighterService.fight(attackerId, defenderId);
 
         VavrAssertions.assertThat(actual).containsLeftInstanceOf(ApplicationError.class);
 
-        verify(spi).findById(attackerId);
+        verify(cardSpi).findById(attackerId);
 
-        verifyNoMoreInteractions(spi);
+        verifyNoMoreInteractions(cardSpi);
     }
 
     @Test
@@ -72,17 +85,17 @@ class CardFighterServiceTest {
 
         val attacker = Card.builder().id(attackerId).hp(10).armor(10).power(10).build();
 
-        when(spi.findById(attackerId)).thenReturn(Option.of(attacker));
-        when(spi.findById(defenderId)).thenReturn(Option.none());
+        when(cardSpi.findById(attackerId)).thenReturn(Option.of(attacker));
+        when(cardSpi.findById(defenderId)).thenReturn(Option.none());
 
         val actual = cardFighterService.fight(attackerId, defenderId);
 
         VavrAssertions.assertThat(actual).containsLeftInstanceOf(ApplicationError.class);
 
-        verify(spi).findById(attackerId);
-        verify(spi).findById(defenderId);
+        verify(cardSpi).findById(attackerId);
+        verify(cardSpi).findById(defenderId);
 
-        verifyNoMoreInteractions(spi);
+        verifyNoMoreInteractions(cardSpi);
     }
 
     @Test
@@ -93,16 +106,16 @@ class CardFighterServiceTest {
         val attacker = Card.builder().id(attackerId).level(10).build();
         val defender = Card.builder().id(defenderId).level(5).build();
 
-        when(spi.findById(attackerId)).thenReturn(Option.of(attacker));
-        when(spi.findById(defenderId)).thenReturn(Option.of(defender));
+        when(cardSpi.findById(attackerId)).thenReturn(Option.of(attacker));
+        when(cardSpi.findById(defenderId)).thenReturn(Option.of(defender));
 
         val actual = cardFighterService.fight(attackerId, defenderId);
 
         VavrAssertions.assertThat(actual).containsLeftInstanceOf(ApplicationError.class);
 
-        verify(spi).findById(attackerId);
-        verify(spi).findById(defenderId);
+        verify(cardSpi).findById(attackerId);
+        verify(cardSpi).findById(defenderId);
 
-        verifyNoMoreInteractions(spi);
+        verifyNoMoreInteractions(cardSpi);
     }
 }
